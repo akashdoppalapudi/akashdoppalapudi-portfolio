da2a1520a4364d624cc693a798cb189e236392a6
Akash Doppalapudi <akashdoppalapudi2001@gmail.com>
2021-08-03 13:32:35 +0530

    added content to landing page

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,28 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
-import NavBar from "../components/NavBar";
+import NavBar from '../components/NavBar';
 
 export default function Home() {
 	return (
 		<div className={styles.container}>
 			<Head>
 				<title>Akash Doppalapudi</title>
-				<meta
-					name="description"
-					content="Portfolio of Akash Doppalapudi"
-				/>
+				<meta name="description" content="Portfolio of Akash Doppalapudi" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main className="page-container">
 				<NavBar />
 				<div className="container">
-					<h2>Landing Page</h2>
+					<div className={styles.greetingBox}>
+						<h2 className={styles.heading}>Hello There!</h2>
+						<h2 className={styles.greetingText}>
+							I am Akash Doppalapudi. I am a Full Stack Developer.
+						</h2>
+						<h2 className={styles.greetingText}>
+							Navigate around the page and know more about me.
+						</h2>
+					</div>
 				</div>
 			</main>
 		</div>
