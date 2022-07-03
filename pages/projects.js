3bdf1bf5f1b155208e2b44dfcc9be898bb69c234
Akash Doppalapudi <akash.d@Technovert.com>
2022-07-03 21:01:12 +0530

    removed ssr in projects page fetching github repos data on page mount

--- pages/projects.js
+++ pages/projects.js
@@ -1,20 +1,17 @@
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 
 import NavBar from '../components/NavBar.js';
 
-export const getServerSideProps = async () => {
-	const res = await fetch(
-		'https://api.github.com/users/akashdoppalapudi/repos'
-	);
-	const data = await res.json();
-	return {
-		props: {
-			repos: data,
-		},
-	};
-};
-
-export const Projects = ({ repos }) => {
+export const Projects = () => {
+	const [repos, setRepos] = useState(null);
+	useEffect(async () => {
+		const res = await fetch(
+			'https://api.github.com/users/akashdoppalapudi/repos'
+		);
+		const data = await res.json();
+		setRepos(data);
+	});
 	return (
 		<div className="bg-gray-900 min-h-screen text-gray-300 p-2 md:p-5">
 			<Head>
@@ -30,21 +27,27 @@ export const Projects = ({ repos }) => {
 							Projects on Github that I&#39;ve worked or have been working on
 						</h2>
 					</div>
-					<div className="block md:grid md:grid-cols-3 mt-4 md:mt-8 gap-4 text-center">
-						{repos.map((repo) => (
-							<div
-								className="m-2 md:col-span-1 cursor-pointer bg-black rounded-md p-8 hover:scale-105 transform transition ease-in-out duration-100 items-center"
-								key={repo.id}
-								onClick={() =>
-									window.open(
-										`https://github.com/akashdoppalapudi/${repo.name}`
-									)
-								}
-							>
-								<h3>{repo.name}</h3>
-							</div>
-						))}
-					</div>
+					{!repos ? (
+						<div className="w-full mt-56 flex justify-center items-center text-xl opacity-50">
+							<p>Loading...</p>
+						</div>
+					) : (
+						<div className="block md:grid md:grid-cols-3 mt-4 md:mt-8 gap-4 text-center">
+							{repos.map((repo) => (
+								<div
+									className="m-2 md:col-span-1 cursor-pointer bg-black rounded-md p-8 hover:scale-105 transform transition ease-in-out duration-100 items-center"
+									key={repo.id}
+									onClick={() =>
+										window.open(
+											`https://github.com/akashdoppalapudi/${repo.name}`
+										)
+									}
+								>
+									<h3>{repo.name}</h3>
+								</div>
+							))}
+						</div>
+					)}
 				</div>
 			</main>
 		</div>
