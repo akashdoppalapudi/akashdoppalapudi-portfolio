5f12e9f944f35854ef0455d07ec025d99f48dc9b
Akash Doppalapudi <akash.d@Technovert.com>
2022-07-03 23:42:26 +0530

    updated tailwind to latest version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-	purge: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
-	darkMode: false, // or 'media' or 'class'
+	content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
 	theme: {
 		extend: {
 			keyframes: {
